d2fdd9a8e5d5bac6f266f183c146b619bcb2513c
Adam <ovadia.adam@gmail.com>
2014-12-26 08:07:45 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ TARGET_RECOVERY_FSTAB = device/asus/grouper/fstab.grouper
 MALLOC_IMPL := dlmalloc
 
 # Custom Kernel Toolchain
-TARGET_GCC_VERSION_ARM := 4.10-sm
+TARGET_GCC_VERSION_ARM := 4.9-sm
 # Clang optimizations
 SYMMETRY := true
 
